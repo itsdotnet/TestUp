83567955cb537890919889366ee4297c05b83257
iskandar kodirov <eiscocyber@gmail.com>
2023-09-06 22:30:05 +0500

    Enums added

---
+++ src/TestUp.Domain/Enums/Level.cs
@@ -0,0 +1,8 @@
+﻿namespace TestUp.Domain.Enums;
+
+public enum Level
+{
+    Easy,
+    Medium,
+    Hard
+}
---
+++ src/TestUp.Domain/Enums/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace TestUp.Domain.Enums;
+
+public enum UserRole
+{
+    Admin,
+    Teacher,
+    User
+}
