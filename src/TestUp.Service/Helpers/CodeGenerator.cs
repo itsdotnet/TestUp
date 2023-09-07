3048c3766aeefebb00e8d0e08a8c2ae1109754ea
iskandar kodirov <eiscocyber@gmail.com>
2023-09-07 09:18:36 +0500

    All helpers writed

---
+++ src/TestUp.Service/Helpers/CodeGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace TestUp.Service.Helpers;
+
+public class CodeGenerator
+{
+    public static int GenerateRandomNumber()
+    {
+        Random random = new Random();
+        return random.Next(100000, 999999);
+    }
+}
