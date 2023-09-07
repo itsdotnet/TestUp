3d1849da873f187135a91dd50f0be98545bb6c0e
iskandar kodirov <eiscocyber@gmail.com>
2023-09-07 16:15:49 +0500

    All missing Service things added

--- src/TestUp.WebApi/Program.cs
+++ src/TestUp.WebApi/Program.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.IO;
+using TestUp.DataAccess.Contexts;
+using TestUp.Service.Helpers;
+using TestUp.WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Database
 
+builder.Services.AddDbContext<TestUpDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// CustomServices
+
+builder.Services.AddServices();
+
+// JWT
+
+builder.Services.AddJwt(builder.Configuration);
+
+// Logger(serilog)
+
+var logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.FromLogContext()
+        .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,6 +41,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Web root Path
+
+PathHelper.WebRootPath = Path.GetFullPath("wwwroot");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
