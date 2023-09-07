0330110f9d542671172ebea418b18d16dd912770
iskandar kodirov <eiscocyber@gmail.com>
2023-09-07 08:57:54 +0500

    Project cleaned

--- src/TestUp.DataAccess/IRepositories/IRepository.cs
+++ src/TestUp.DataAccess/IRepositories/IRepository.cs
@@ -6,14 +6,9 @@ namespace TestUp.DataAccess.IRepositories;
 public interface IRepository<T> where T : Auditable
 {
     Task<T> AddAsync(T entity);
-
     Task<T> UpdateAsync(T entity);
-
     Task<bool> DeleteAsync(Expression<Func<T, bool>> expression);
-
     Task<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null);
-
     IQueryable<T> SelectAll(Expression<Func<T, bool>> expression = null, string[] includes = null, bool isTracking = true);
-
     Task SaveAsync();
 }
