a2280ca46f240968d9f2767cc6d1037cf7909ae7
Ferganskiy <odilovmadiyor029@gmail.com>
2023-09-07 12:38:15 +0500

    ResultService's interface added

---
+++ src/TestUp.Service/Interfaces/IResultService.cs
@@ -0,0 +1,16 @@
+﻿using TestUp.Service.DTOs.Answer;
+using TestUp.Service.DTOs.Result;
+
+namespace TestUp.Service.Interfaces;
+
+public interface IResultService
+{
+    Task<bool> DeleteAsync(long id);
+    Task<IEnumerable<Result>> GetAllAsync();
+    Task<ResultResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<Result>> GetByExamId(long examId);
+    Task<IEnumerable<Result>> GetByUserId(long userId);
+    Task<float> MyScore(long UserId, long ExamId);
+    Task<ResultResultDto> UpdateAsync(ResultUpdateDto resultUpdate);
+    Task<ResultResultDto> CreateAsync(ResultCrestionDto resultCreation);
+}
