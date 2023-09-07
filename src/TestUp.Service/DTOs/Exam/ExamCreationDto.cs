75dc4c1c6aa50c296a086468d89d2f4dc8079190
Ferganskiy <odilovmadiyor029@gmail.com>
2023-09-07 08:58:51 +0500

    ExamDto added

---
+++ src/TestUp.Service/DTOs/Exam/ExamCreationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TestUp.Service.DTOs.Exam;
+
+public class ExamCreationDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Password { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
