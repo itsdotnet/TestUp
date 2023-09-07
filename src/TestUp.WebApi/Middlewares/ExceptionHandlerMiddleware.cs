1bfe0d22b89eb7bf1790d3066acf33abbea2d9bc
iskandar kodirov <eiscocyber@gmail.com>
2023-09-07 16:19:53 +0500

    Exception Handler Middleware added

---
+++ src/TestUp.WebApi/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using TestUp.WebApi.Models;
+using System.Threading.Tasks;
+using TestUp.Service.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace TestUp.WebApi.Middlewares;
+
+public class ExceptionHandlerMiddleware
+{
+    public readonly RequestDelegate _request;
+    public readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+    public ExceptionHandlerMiddleware(RequestDelegate request, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        _request = request;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _request(context);
+        }
+        catch (NotFoundException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (AlreadyExistException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (CustomException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = 500;
+            _logger.LogError(ex.ToString());
+
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message,
+            });
+        }
+    }
+}
