70ff38641e21281906d81a0bdfbbc4935714ebd9
iskandar kodirov <eiscocyber@gmail.com>
2023-09-07 10:06:39 +0500

    Fixed

--- src/TestUp.Domain/Commons/Auditable.cs
+++ src/TestUp.Domain/Commons/Auditable.cs
@@ -6,6 +6,6 @@ public class Auditable
 {
     public long Id { get; set; }
     public DateTime CreatedAt { get; set; } = Time.GetCurrentTime();
-    public DateTime UpdatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public bool IsDeleted { get; set; }
 }
