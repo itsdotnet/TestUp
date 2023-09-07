d1498f3e2cf353f6260623467f83d3caf981693c
iskandar kodirov <eiscocyber@gmail.com>
2023-09-07 15:31:55 +0500

    Response model writed

---
+++ src/TestUp.WebApi/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace TestUp.WebApi.Models;
+
+public class Response
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public object Data { get; set; }
+}
