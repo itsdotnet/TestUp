5ff0dd67b2ce7cce0b3245e95b3e0a652bf94f02
iskandar kodirov <eiscocyber@gmail.com>
2023-09-07 09:01:22 +0500

    Exceptions Added

---
+++ src/TestUp.Service/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+﻿namespace TestUp.Service.Exceptions;
+
+public class CustomException : Exception
+{
+    public CustomException(int statusCode, string message) : base(message)
+    {
+        this.StatusCode = statusCode;
+    }
+    
+    public int StatusCode { get; set; }
+}
---
+++ src/TestUp.Service/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace TestUp.Service.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message)
+    { }
+
+    public NotFoundException(string message, Exception innerException) : base(message, innerException)
+    { }
+
+    public int StatusCode { get; set; } = 404;
+}
