b695ce09a662826455a6338a57e175a06899de98
iskandar kodirov <eiscocyber@gmail.com>
2023-09-07 09:06:47 +0500

    Pagination added and other extensions writed

---
+++ src/TestUp.Service/Extensions/Converter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TestUp.Service.Extensions;
+
+public static class Converter
+{
+    public static byte[] ToByte(this IFormFile formFile)
+    {
+        using var memoryStream = new MemoryStream();
+        formFile.CopyTo(memoryStream);
+        return memoryStream.ToArray();
+    }
+}
