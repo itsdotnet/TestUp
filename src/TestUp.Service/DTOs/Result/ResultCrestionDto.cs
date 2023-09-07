b7c2d705aed4fe3539b732b12fbafca6bdbfdfed
Ferganskiy <odilovmadiyor029@gmail.com>
2023-09-07 09:24:50 +0500

    ResultDto and UserDto added

---
+++ src/TestUp.Service/DTOs/Result/ResultCrestionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TestUp.Service.DTOs.Result;
+
+public class ResultCrestionDto
+{
+    public long UserId { get; set; }
+    public long ExamId { get; set; }
+    public float Score { get; set; }
+}
+
---
+++ src/TestUp.Service/DTOs/Result/ResultResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TestUp.Service.DTOs.Result;
+
+public class ResultResultDto
+{
+    public User User { get; set; }
+
+    public Exam Exam { get; set; }
+
+    public float Score { get; set; }
+}
+
