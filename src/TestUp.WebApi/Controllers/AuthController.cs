10c462ef5edc5dca08cb46d600e9337785b68c21
iskandar kodirov <eiscocyber@gmail.com>
2023-09-07 19:08:31 +0500

    AuthController writed

---
+++ src/TestUp.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestUp.Service.Interfaces;
+using TestUp.WebApi.Controllers;
+using TestUp.WebApi.Models;
+
+public class AuthController : BaseController
+{
+    private readonly IAuthService authService;
+
+    public AuthController(IAuthService authService)
+    {
+        this.authService = authService;
+    }
+
+    [HttpPost("authenticate")]
+    public async Task<IActionResult> AuthenticateAsync(string emailOrUsername, string password)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.authService.GenerateAndCacheTokenAsync(emailOrUsername, password)
+        });
+    }
+
+}
