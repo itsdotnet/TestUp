a5ebc48aeac9e5c2a8f7e233f8fb5ec3aba96423
Ferganskiy <odilovmadiyor029@gmail.com>
2023-09-07 12:02:01 +0500

    ExamService's Interface added

---
+++ src/TestUp.Service/Interfaces/IExamService.cs
@@ -0,0 +1,18 @@
+﻿using TestUp.Service.DTOs.Answer;
+using TestUp.Service.DTOs.Exam;
+
+namespace TestUp.Service.Interfaces;
+
+public interface IExamService
+{
+    Task DeleteAsync(long id);
+    Task<IEnumerable<Exam>> GetAllAsync();
+    Task<ExamResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<ExamResultDto>> EndedExams();
+    Task<IEnumerable<ExamResultDto>> FutureExams();
+    Task<IEnumerable<ExamResultDto>> CurrentExams();
+    Task<IEnumerable<ExamResultDto>> GetByTitleAsync(string title);
+    Task<ExamResultDto> UpdateAsync(ExamUpdateDto examUpdate);
+    Task<ExamResultDto> CreateAsync(AnswerCreationDto answerCreation);
+    Task<IEnumerable<ExamResultDto>> GetByNearExamAsync(DateTime dateTime);
+}
