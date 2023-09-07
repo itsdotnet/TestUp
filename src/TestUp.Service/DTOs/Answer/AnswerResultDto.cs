7322107614b103915c654ed9c6a1f0ef4aa337a9
Ferganskiy <odilovmadiyor029@gmail.com>
2023-09-07 08:55:08 +0500

    AnswerDto added

---
+++ src/TestUp.Service/DTOs/Answer/AnswerResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TestUp.Service.DTOs.Answer;
+
+public class AnswerResultDto
+{
+    public long Id { get; set; }
+    public long QuestionId { get; set; }
+    public long? AttachmentId { get; set; }
+    public string Text { get; set; }
+}
