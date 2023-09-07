7d23b88e6b5abe354325643e9c568d47abf7b618
Ferganskiy <odilovmadiyor029@gmail.com>
2023-09-07 09:02:34 +0500

    PermissionDto added

--- src/TestUp.Service/DTOs/Exam/ExamResultDto.cs
+++ src/TestUp.Service/DTOs/Exam/ExamResultDto.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace TestUp.Service.DTOs.Exam;
 
-namespace TestUp.Service.DTOs.Exam
+public class ExamResultDto
 {
-    internal class ExamResultDto
-    {
-    }
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Password { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
 }
---
+++ src/TestUp.Service/DTOs/Permission/PermissionResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TestUp.Service.DTOs.Permission;
+
+public class PermissionResultDto
+{
+    public long Id { get; set; }
+    public bool Get { get; set; }
+    public bool Create { get; set; }
+    public bool Update { get; set; }
+    public bool Delete { get; set; }
+}
