1c44aabbd7c3d4a3dde25301c0d5636d37a187d9
JSobirov <jasurbey2@gmail.com>
2023-09-07 11:57:18 +0500

    AttachmentInterface added

---
+++ src/TestUp.Service/Interfaces/IAttachmentService.cs
@@ -0,0 +1,9 @@
+﻿using TestUp.Service.DTOs.Attachment;
+
+namespace TestUp.Service.Interfaces;
+
+public interface IAttachmentService
+{
+    Task<Attachment> UploadAsync(AttachmentCreationDto dto);
+    Task<bool> RemoveAsync(Attachment attachment);
+}
