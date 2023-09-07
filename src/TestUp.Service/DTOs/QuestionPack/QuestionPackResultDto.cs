491d91468fd7eb8f1eef1b789193b6d72ab6bcec
Ferganskiy <odilovmadiyor029@gmail.com>
2023-09-07 09:17:03 +0500

    QuestionPackDto added

---
+++ src/TestUp.Service/DTOs/QuestionPack/QuestionPackResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TestUp.Service.DTOs.QuestionPack;
+
+public class QuestionPackResultDto
+{
+    public long Id { get; set; }
+    public Question Question { get; set; }
+    public Exam Exam { get; set; }
+}
