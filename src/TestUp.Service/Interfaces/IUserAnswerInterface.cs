f3146527b4de90a92fac5a250580b0ba0ad05976
abbosZoirov1 <122oszoirov@gmail.com>
2023-09-07 15:45:23 +0500

    Added UserAnswer service

--- src/TestUp.Service/Interfaces/IUserAnswerInterface.cs
+++ src/TestUp.Service/Interfaces/IUserAnswerInterface.cs
@@ -5,11 +5,11 @@ namespace TestUp.Service.Interfaces;
 public interface IUserAnswerInterface
 {
     Task<bool> DeleteAsync(long id);
-    Task<IEnumerable<UserAnswer>> GetAllAsync();
+    Task<IEnumerable<UserAnswerResultDto>> GetAllAsync();
     Task<UserAnswerResultDto> GetByIdAsync(long id);
-    Task<IEnumerable<UserAnswer>> GetByExamIdAsync(long examId);
-    Task<IEnumerable<UserAnswer>> GetByUserIdAsync(long userId);
-    Task<IEnumerable<UserAnswer>> GetByAnswerIdAsync(long answerId);
+    Task<IEnumerable<UserAnswerResultDto>> GetByExamIdAsync(long examId);
+    Task<IEnumerable<UserAnswerResultDto>> GetByUserIdAsync(long userId);
+    Task<IEnumerable<UserAnswerResultDto>> GetByAnswerIdAsync(long answerId);
     Task<UserAnswerResultDto> ModifyAsync(UserAnswerUpdateDto userAnswerUpdate);
     Task<UserAnswerResultDto> CreateAsync(UserAnswerCreationDto userAnswerCreation);
 }
