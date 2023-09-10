1f4f62485c63403f2530c97bb4afbb824f5c4195
iskandar kodirov <eiscocyber@gmail.com>
2023-09-10 12:00:53 +0500

    User  Dto fixed

--- src/TestUp.Service/DTOs/User/UserCreationDto.cs
+++ src/TestUp.Service/DTOs/User/UserCreationDto.cs
@@ -9,6 +9,5 @@ public class UserCreationDto
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public UserRole UserRole { get; set; }
     public long? AttachmentId { get; set; }
 }
--- src/TestUp.Service/DTOs/User/UserResultDto.cs
+++ src/TestUp.Service/DTOs/User/UserResultDto.cs
@@ -5,11 +5,11 @@ namespace TestUp.Service.DTOs.User;
 
 public class UserResultDto
 {
+    public long Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
     public UserRole UserRole { get; set; }
     public AttachmentResultDto Attachment { get; set; }
 }
